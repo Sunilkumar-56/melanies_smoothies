fa3bf21ec4e96bd28ed185872ae12deda211fec5
Sunilkumar-56 <sunilkumar.sivakumar@saama.com>
2024-07-23 18:07:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
+
 
 
 st.title("Customize your smoothie :cup_with_straw:")
