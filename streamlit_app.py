a5fc333f2f2fec5d7365908147165fdcbdb0c0d2
Sunilkumar-56 <sunilkumar.sivakumar@saama.com>
2024-07-31 20:04:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,62 +1,67 @@
 # Import python packages
-import requests
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-
-
-st.title("Customize your smoothie :cup_with_straw:")
+# Write directly to the app
+st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom smoothie
-     """
- )
+    """Choose the fruits you want in your custom Smoothie!
+    """
+)
 
-
-name_on_order=st.text_input('Name on Smoothie:')
-st.write('The name on the smoothie will be ',name_on_order)
-
-
+# Remove the SelectBox
 # option = st.selectbox(
-#      'What is your favorite fruit?',
+#     "What is your favorite fruit?",
 #     ("Banana", "Strawberries", "Peaches"))
 
-# st.write("Your favorite fruit is :", option)
+# st.write("Your favorite fruit is:", option)
 
-cnx=st.connection("snowflake")
-session = cnx.session()
+# Add a Name Box for Smoothie Orders
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-pd_df=my_dataframe.to_pandas()
-ingredients_list=st.multiselect('Choose upto five ingredients:',my_dataframe,max_selections=5)
+# Display the Fruit Options List in Your Streamlit in Snowflake (SiS) App. 
+# If the line session = get_active_session() appears in your code two times, delete one of the lines.
+session = get_active_session()
+# Add the New SEARCH_ON Column to the Dataframe that feeds the Multiselect
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
+# # Add a Multiselect
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections = 5
+)
+
+# Our ingredients variable is an object or data type called a LIST. So it's a list in the traditional sense of the word, but it is also a datatype or object called a LIST. A LIST is different than a DATAFRAME which is also different from a STRING!
 if ingredients_list:
-
-    ingredients_string=' '
-
+    # st.write(ingredients_list)
+    # st.text(ingredients_list)
+    
+    # Create the INGREDIENTS_STRING Variable
+    ingredients_string = ''
+    
+    # Add the FOR Block
     for fruit_chosen in ingredients_list:
-        ingredients_string+=fruit_chosen+' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        ingredients_string += fruit_chosen + ' '
         
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
-        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-
+    # Output the String 
     # st.write(ingredients_string)
-   # my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
+    # Build a SQL Insert Statement & Test It
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '"""+ name_on_order +"""')"""
     st.write(my_insert_stmt)
-    
-    
-    time_to_insert=st.button('Submit Order')
-    
+    # for troubleshooting
+    # st.stop()
+
+    # Add a Submit Button
+    time_to_insert = st.button('Submit Order')
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        
-        st.success('Your Smoothie is ordered!', icon="✅")
-        
+        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
